fa1c7e92ef1adb0601510e273407fdd29b454eb4
shishira-v-kasal <shishira@usf.edu>
2023-04-19 14:30:58 -0400

    main file

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, jsonify, request
+from services.data_service import DataService
+
+app = Flask(__name__)
+
+dataService = DataService()
+
+@app.route("/")
+@app.route("/home")
+def home():
+    
+    return render_template('home.html', data=dataService.getData(), upcoming_events=dataService.getUpcomingEvents())
+
+
+@app.route('/view', methods=['GET'])
+def view():
+    return render_template('view_list.html', data=dataService.getAllJobData())
+
+@app.route('/addNewJob', methods=['POST'])
+def addNewJob():
+    job_data = request.get_json()
+    dataService.addJob(job_data)
+    response = {"status": "Success"}
+    return jsonify(response)
+
+@app.route('/deleteJob', methods=['POST'])
+def delete():
+    job_data = request.get_json()
+    dataService.deleteJob(job_id=job_data['job_id'])
+    print('delete called')
+    response = {"status": "Success"}
+    return jsonify(response)
+
+@app.route('/updateJobStatus', methods=['POST'])
+def updateJobStatus():
+    job_data = request.get_json()
+    print(job_data)
+    job_id = job_data['job_id']
+    job_status = job_data['job_status']
+    dataService.updateJobStatus(job_id, newStatus=job_status)
+    response = {"status": job_status}
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=7777)
